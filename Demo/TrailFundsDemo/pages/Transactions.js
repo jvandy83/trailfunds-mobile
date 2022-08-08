dc56e1b934178b2bced34ffc68062d6dff02b73b
Blake Kohlbrecher <blakekohlbrecher@gmail.com>
2022-08-08 15:26:31 +0000

    Resolve "Create App Skeleton"

---
+++ Demo/TrailFundsDemo/pages/Transactions.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This page will handle transaction process, like asking the user for their payment information that will be sent to Braintree</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

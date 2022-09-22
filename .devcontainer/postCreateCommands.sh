2398118af08ba1cbd5a6a8baf54b8dc737ed23e5
Jacob Hansen <2000.jihansen@gmail.com>
2022-09-22 16:53:24 -0600

    Updated postCreatecommands and Pipeline fix

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,30 +1,25 @@
 cd ./App/
 echo "Creating .venv (obsolete)..."
 python3 -m venv .venv
-. .venv/bin/activate
-echo ".venv created under app/"
+# . .venv/bin/activate
 echo "Upgrading pip..."
 pip install --upgrade pip
-echo "Pip upgraded"
 echo "Installing project requirements (requirements.txt, requirements_dev.txt)..."
-python3 -m pip install -r requirements.txt 
-python3 -m pip install -r requirements_dev.txt
-echo "Requirements installed"
+python3 -m pip install -r ../app/backend/requirements.txt 
+python3 -m pip install -r ../app/backend/requirements_dev.txt
 echo "Installing lint tools..."
 pip install black
 pip install flake8
 pip install autoflake
 pip install pylint
-echo "Lint tools installed"
 echo "Running 'npm install'..."
-npm install -g
-echo "Done"
-echo "Installing Expo..."
-npm install -g expo-cli
-echo "Expo installed"
+npm install
+# echo "Installing Expo..."
+# npm install -g expo-cli
 echo "Running 'expo start'..."
 expo start
 echo """
 Post Commands Completed. 
+There may be errors in the startup process, report any unexpected behavior.
 If .venv is not visible under app/, press the refresh icon.
 """

fefa1c710803a6f2198cc82c5c9708831654b7a6
jvandy83 <vanthedev@gmail.com>
2023-04-28 15:01:29 -0600

    added setup for push notifications

--- App/frontend/App.js
+++ App/frontend/App.js
@@ -6,14 +6,51 @@ import 'expo-dev-client';
 
 import { StripeProvider } from '@stripe/stripe-react-native';
 
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 import { StackHome } from './routeNavigation/stack'; //Importing the main stack of pages for the app
 
 import { Provider } from 'react-redux';
 import { store } from './reduxStore';
 
+import * as Notifications from 'expo-notifications';
+
+import {
+	sendPushNotification,
+	registerForPushNotificationsAsync,
+} from './pushNotifications';
+
 export default function App() {
+	const [expoPushToken, setExpoPushToken] = useState('');
+	const [notification, setNotification] = useState(false);
+	const notificationListener = useRef();
+	const responseListener = useRef();
+
+	useEffect(() => {
+		registerForPushNotificationsAsync().then((token) =>
+			setExpoPushToken(token),
+		);
+
+		notificationListener.current =
+			Notifications.addNotificationReceivedListener((notification) => {
+				setNotification(notification);
+			});
+
+		responseListener.current =
+			Notifications.addNotificationResponseReceivedListener((response) => {
+				console.log(response);
+			});
+
+		return () => {
+			Notifications.removeNotificationSubscription(
+				notificationListener.current,
+			);
+			Notifications.removeNotificationSubscription(responseListener.current);
+		};
+	}, []);
+
+	console.log('expo push token is: ', expoPushToken);
+
 	return (
 		<StripeProvider
 			merchantIdentifier='merchant.com.trailfunds'
